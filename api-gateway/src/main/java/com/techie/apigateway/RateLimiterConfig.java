208803a43a29174a8f47ba3c16e6adcdefdc0ee1
Chetan Macbook <chetanmacbook@Chetans-MacBook-Air.local>
2026-02-24 20:52:43 +0530

    fix nginx

--- api-gateway/src/main/java/com/techie/apigateway/RateLimiterConfig.java
+++ api-gateway/src/main/java/com/techie/apigateway/RateLimiterConfig.java
@@ -1,8 +1,9 @@
 package com.techie.apigateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -10,11 +11,27 @@ public class RateLimiterConfig {
 
     @Bean
     public KeyResolver userKeyResolver() {
-        return exchange -> Mono.just(
-                exchange.getRequest()
-                        .getRemoteAddress()
-                        .getAddress()
-                        .getHostAddress()
-        );
+        return exchange -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+
+            // 🔥 Get real client IP from Nginx
+            String forwardedFor = request.getHeaders().getFirst("X-Forwarded-For");
+
+            if (forwardedFor != null && !forwardedFor.isEmpty()) {
+                return Mono.just(forwardedFor.split(",")[0]);
+            }
+
+            // fallback to remote address
+            if (request.getRemoteAddress() != null) {
+                return Mono.just(
+                        request.getRemoteAddress()
+                                .getAddress()
+                                .getHostAddress()
+                );
+            }
+
+            return Mono.just("unknown");
+        };
     }
 }
